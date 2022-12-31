3b234761238f9c6dec44eb57502aae5855c1c2db
sydist <sydist.private+git@gmail.com>
2022-12-31 16:53:04 +0200

    feat: add implementation

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+#!/usr/bin/env node
 
+// Loads .env file contents into process.env.
+import { config } from "dotenv";
+config();
+
+const HEADERS = { "authorization": process.env.TOKEN, "content-type": "application/json" };
+const FRIENDS = await fetch("https://discord.com/api/v9/users/@me/relationships",
+{
+    method: "GET",
+    headers: HEADERS,
+}).then(res => res.json());
+
+for (const FRIEND of FRIENDS)
+{
+
+    const MESSAGE = process.argv.slice(2)[0].replaceAll("{name}", FRIEND.user.username);
+    fetch(`https://discord.com/api/v9/channels/${FRIEND.id}/messages`,
+    {
+        method: "POST",
+        headers: HEADERS,
+        body: JSON.stringify({ content: MESSAGE }),  
+    });
+}
